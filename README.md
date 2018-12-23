b46a4825abcb6399fb40a8d1697955642249dc06
hokuto_Hirano <p238058h@gmail.com>
2018-12-24 07:05:37 +0900

    save

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # サンプリングデータ作成
 
-params.jsonのパラメタリストを編集する。
+params.jsonのパラメタリストを編集する。  
 k_aveは偶数になるように！
 
-python make-params.py
-全パラメタ組み合わせファイル(resource/params-all.csv)と
-最大サンプリング行のみファイル(resource/params-max-sampling.csv)を作成する。
+sh bin/make-dir.sh  
+保存用ディレクトリを作成する(デフォルト30iter)。
 
-Rscript make-data-1.R | tee resource/log/make-data-1.log
+mkdir resource/tmp  
+ディレクトリ作成。
+
+python make-params.py  
+全パラメタ組み合わせファイル(resource/params-all.csv)と  
+最大サンプリング行のみファイル(resource/tmp/params-max-sampling.csv)を作成する。
+
+Rscript make-data-1.R | tee resource/log/make-data-1.log  
 最大サンプリングの条件のみ計算する。
 
-Rscript make-data-2.R | tee resource/log/make-data-2.log
+Rscript make-data-2.R | tee resource/log/make-data-2.log  
 最大サンプリングよりサブサンプリングする。
 
 
@@ -18,72 +24,74 @@ Rscript make-data-2.R | tee resource/log/make-data-2.log
 
 ## mic
 
-sh mkdir.sh
-保存用ディレクトリを作成する。
-ファイル内で、iter_numを指定する。
+sh mkdir.sh  
+保存用ディレクトリを作成する(デフォルト30iter)。  
 
-Rscript main.R | tee out-res/main.log
-計算する。
+Rscript main.R | tee out-res/main.log  
+相関分析。
 
-python mapping-save-data.py | tee out-res/mapping.log
-iter毎のaucをまとめる。
+python mapping-save-data.py count-mic | tee out-res/mapping-count-mic.log  
+python mapping-save-data.py flac-mic | tee out-res/mapping-flac-mic.log  
+相関分析結果csvファイルの作成。
 
 ## spiec-easi
 
-sh mkdir.sh
-保存用ディレクトリを作成する。
-ファイル内で、iter_numを指定する。
+sh mkdir.sh  
+保存用ディレクトリを作成する(デフォルト30iter)。  
 
 Rscript main.R | tee out-res/main.log
-計算する。
+相関分析。
 
-python mapping-save-data.py | tee out-res/mapping.log
-iter毎のaucをまとめる。
+python mapping-save-data.py | tee out-res/mapping.log  
+相関分析結果csvファイルの作成。
 
 ## rebacca
 
-sh mkdir.sh
-保存用ディレクトリを作成する。
-ファイル内で、iter_numを指定する。
+sh mkdir.sh  
+保存用ディレクトリを作成する(デフォルト30iter)。
 
 Rscript main.R | tee out-res/main.log
-計算する。
+相関分析。
 
-python mapping-save-data.py | tee out-res/mapping.log
-iter毎のaucをまとめる。
+python mapping-save-data.py | tee out-res/mapping.log  
+相関分析結果csvファイルの作成。
 
 ## cclasso
 
-sh mkdir.sh
-保存用ディレクトリを作成する。
-ファイル内で、iter_numを指定する。
+sh mkdir.sh  
+保存用ディレクトリを作成する(デフォルト30iter)。
 
-Rscript main.R | tee out-res/main.log
-計算する。
+Rscript main.R | tee out-res/main.log  
+相関分析。
 
-python mapping-save-data.py | tee out-res/mapping.log
-iter毎のaucをまとめる。
+python mapping-save-data.py | tee out-res/mapping.log  
+相関分析結果csvファイルの作成。
 
 ## general
 
-sh mkdir.sh
-保存用ディレクトリを作成する。
-ファイル内で、iter_numを指定する。
+sh mkdir.sh  
+保存用ディレクトリを作成する(デフォルト30iter)。
 
-Rscript main.R | tee out-res/main.log
-計算する。
+Rscript main.R | tee out-res/main.log  
+相関分析。
 
-python mapping-save-data.py | tee out-res/mapping.log
-iter毎のaucをまとめる。
+python mapping-save-data.py count-pea | tee out-res/mapping.log  
+python mapping-save-data.py count-spe | tee out-res/mapping.log  
+python mapping-save-data.py count-ppea | tee out-res/mapping.log  
+python mapping-save-data.py count-pspe | tee out-res/mapping.log  
+python mapping-save-data.py flac-pea | tee out-res/mapping.log  
+python mapping-save-data.py flac-spe | tee out-res/mapping.log  
+python mapping-save-data.py flac-ppea | tee out-res/mapping.log  
+python mapping-save-data.py flac-pspe | tee out-res/mapping.log  
+相関分析結果csvファイルの作成。
 
 ## sparcc
 
-sh clean-mkdir.sh
-保存用ディレクトリを作成する。
-ファイル内で、iter_numを指定する。
+sh clean-mkdir.sh  
+保存用ディレクトリを作成する(デフォルト30iter)。
 
-Rscript main.R | tee out-res/main.log
-計算する。
+Rscript main.R | tee out-res/main.log  
+相関分析。
 
-python mapping-save-data.py | tee out-res/mapping.log
-iter毎のaucをまとめる。
+python mapping-save-data.py | tee out-res/mapping.log  
+相関分析結果csvファイルの作成。  
--- method/general/mapping-save-data.py
+++ method/general/mapping-save-data.py
@@ -2,6 +2,7 @@ import json
 import itertools
 import pandas as pd
 import numpy as np
+import sys
 
 
 
@@ -11,100 +12,34 @@ j_data = json.load(json_path)
 
 iter_num = j_data['iter-num']
 
-# 保存用ファイルを読み込んでgeneralだけを抽出する
+# メソッド種類
+args = sys.argv
+method = args[1]
+
+# 保存用ファイルを読み込んでmicだけを抽出する
 df = pd.read_csv(j_data['params-for-res-csv'])
-df_save = df[(df['calc-method'] == 'count-pea') |
-             (df['calc-method'] == 'count-spe') |
-             (df['calc-method'] == 'count-ppea') |
-             (df['calc-method'] == 'count-pspe') |
-             (df['calc-method'] == 'flac-pea') |
-             (df['calc-method'] == 'flac-spe') |
-             (df['calc-method'] == 'flac-ppea') |
-             (df['calc-method'] == 'flac-pspe')]
+df_save = df[df['calc-method'] == method]
 
 # 基準paramsの読み込み
 params = pd.read_csv(j_data['params-all-csv'])
+
+
 
 # paramsに従って、１行ずつaucをマッピングしていく
 for i in range(len(params)):
     id = params.iloc[i]['id']
 
     for iter in range(1, iter_num + 1):
+        auc_file = 'out-res/iter-{}/id-{}-{}.txt'.format(iter, id, method)
 
-        # @@@@@ 1 count-pea @@@@@
-        auc_file_1 = 'out-res/iter-{}/id-{}-count-pea.txt'.format(iter, id)
         try:
-            auc_1 = np.loadtxt(auc_file_1)
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'count-pea') & (df_save['auc-type'] == 'roc'), 'iter-{}'.format(iter)] = auc_1[0]
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'count-pea') & (df_save['auc-type'] == 'prc'), 'iter-{}'.format(iter)] = auc_1[1]
-        except:
-            print('\n\n', auc_file_1, ' ない\n\n')
+            auc = np.loadtxt(auc_file)
 
-        # @@@@@ 2 count-spe @@@@@
-        auc_file_2 = 'out-res/iter-{}/id-{}-count-spe.txt'.format(iter, id)
-        try:
-            auc_2 = np.loadtxt(auc_file_2)
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'count-spe') & (df_save['auc-type'] == 'roc'), 'iter-{}'.format(iter)] = auc_2[0]
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'count-spe') & (df_save['auc-type'] == 'prc'), 'iter-{}'.format(iter)] = auc_2[1]
-        except:
-            print('\n\n', auc_file_2, ' ない\n\n')
+            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == method) & (df_save['auc-type'] == 'roc'), 'iter-{}'.format(iter)] = auc[0]
 
-        # @@@@@ 3 count-ppea @@@@@
-        auc_file_3 = 'out-res/iter-{}/id-{}-count-ppea.txt'.format(iter, id)
-        try:
-            auc_3 = np.loadtxt(auc_file_3)
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'count-ppea') & (df_save['auc-type'] == 'roc'), 'iter-{}'.format(iter)] = auc_3[0]
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'count-ppea') & (df_save['auc-type'] == 'prc'), 'iter-{}'.format(iter)] = auc_3[1]
+            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == method) & (df_save['auc-type'] == 'prc'), 'iter-{}'.format(iter)] = auc[1]
         except:
-            print('\n\n', auc_file_3, ' ない\n\n')
-
-        # @@@@@ 4 count-pspe @@@@@
-        auc_file_4 = 'out-res/iter-{}/id-{}-count-pspe.txt'.format(iter, id)
-        try:
-            auc_4 = np.loadtxt(auc_file_4)
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'count-pspe') & (df_save['auc-type'] == 'roc'), 'iter-{}'.format(iter)] = auc_4[0]
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'count-pspe') & (df_save['auc-type'] == 'prc'), 'iter-{}'.format(iter)] = auc_4[1]
-        except:
-            print('\n\n', auc_file_4, ' ない\n\n')
-
-        # @@@@@ 5 flac-pea @@@@@
-        auc_file_5 = 'out-res/iter-{}/id-{}-flac-pea.txt'.format(iter, id)
-        try:
-            auc_5 = np.loadtxt(auc_file_5)
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'flac-pea') & (df_save['auc-type'] == 'roc'), 'iter-{}'.format(iter)] = auc_5[0]
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'flac-pea') & (df_save['auc-type'] == 'prc'), 'iter-{}'.format(iter)] = auc_5[1]
-        except:
-            print('\n\n', auc_file_5, ' ない\n\n')
-
-        # @@@@@ 6 flac-spe @@@@@
-        auc_file_6 = 'out-res/iter-{}/id-{}-flac-spe.txt'.format(iter, id)
-        try:
-            auc_6 = np.loadtxt(auc_file_6)
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'flac-spe') & (df_save['auc-type'] == 'roc'), 'iter-{}'.format(iter)] = auc_6[0]
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'flac-spe') & (df_save['auc-type'] == 'prc'), 'iter-{}'.format(iter)] = auc_6[1]
-        except:
-            print('\n\n', auc_file_6, ' ない\n\n')
-        
-        # @@@@@ 7 flac-ppea @@@@@
-        auc_file_7 = 'out-res/iter-{}/id-{}-flac-ppea.txt'.format(iter, id)
-        try:
-            auc_7 = np.loadtxt(auc_file_7)
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'flac-ppea') & (df_save['auc-type'] == 'roc'), 'iter-{}'.format(iter)] = auc_7[0]
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'flac-ppea') & (df_save['auc-type'] == 'prc'), 'iter-{}'.format(iter)] = auc_7[1]
-        except:
-            print('\n\n', auc_file_7, ' ない\n\n')
-        
-        # @@@@@ 8 flac-pspe @@@@@
-        auc_file_8 = 'out-res/iter-{}/id-{}-flac-pspe.txt'.format(iter, id)
-        try:
-            auc_8 = np.loadtxt(auc_file_8)
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'flac-pspe') & (df_save['auc-type'] == 'roc'), 'iter-{}'.format(iter)] = auc_8[0]
-            df_save.loc[(df_save['id'] == id) & (df_save['calc-method'] == 'flac-pspe') & (df_save['auc-type'] == 'prc'), 'iter-{}'.format(iter)] = auc_8[1]
-        except:
-            print('\n\n', auc_file_8, ' ない\n\n')
+            print('\n\n', auc_file, ' ない\n\n')
 
 # 保存
-df_save.to_csv('res-general.csv', index=False)
-        
-
-
+df_save.to_csv('res-{}.csv'.format(method), index=False)
